d54a9eb636839447408db9b27ff6519f39667b4d
Erick Amorim <101286204+Amorim-EA@users.noreply.github.com>
2023-08-04 14:51:33 -0400

    script com mensagem de erro personalizavel

--- js/script.js
+++ js/script.js
@@ -4,3 +4,19 @@ let requisicao = new XMLHttpRequest();
 requisicao.open( "GET", requisicao_URL );
 requisicao.responseType = "json";
 requisicao.send();  
+
+class Mensagem_Error extends Error{
+  constructor( message ){
+    super( message )
+    this.name =  "Error Message: ";
+  }
+  
+  get_Message(){
+    
+        return `<div id="container-noticias">
+                <h2>${this.name}</h2>
+                <p>${this.message}</p>
+                </div>`;
+    
+  }
+}
