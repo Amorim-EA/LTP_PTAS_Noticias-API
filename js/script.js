febc5b7e1b6f35b5b0ee8ba645c3b163112c150f
Erick Amorim <101286204+Amorim-EA@users.noreply.github.com>
2023-08-04 14:52:52 -0400

    adicionei a classe das noticias

--- js/script.js
+++ js/script.js
@@ -20,3 +20,95 @@ class Mensagem_Error extends Error{
     
   }
 }
+
+class Noticia_Destaque{
+ 
+  set_Atributos_Destaque( imagem , data , titulo , autor , resumo , url ){
+    this.imagem = imagem;
+    this.data = data;
+    this.titulo = titulo;
+    this.autor = autor; 
+    this.resumo = resumo;
+    this.url = url;
+   }
+   
+  set_Noticia_Destaque(){
+     
+if( this.imagem == undefined || this.title == undefined || this.data == undefined || this.autor == undefined || this.resumo == undefined || this.url == undefined ){
+  
+  throw new Mensagem_Error( "Alguns dados na API json podem estar indefinidos" );
+       
+  }else{
+       
+ return `<div id="container-noticia_destaque">
+          <center><img src="${this.imagem}" id="imagem_destaque"></center>
+          <h3>${this.titulo}</h3>
+          <h5>Publicado: ${this.data}</h1>
+          <h5>Autor: ${this.autor}</h1>
+          <p>${this.resumo}</p>
+          <center><a href="${this.url}">Clique aqui para ver a noticia completa</a></center>
+         </div>`;
+  }   
+}
+
+ mostrar_Noticia_Destaque(){
+   
+ try{
+     let id = document.getElementById("noticias");
+     return id.insertAdjacentHTML( 'afterbegin' , this.set_Noticia_Destaque() );
+    
+    }catch(error){
+   
+     let id = document.getElementById("noticias");
+     return id.insertAdjacentHTML( 'afterbegin' , error.get_Message() );
+    
+    }
+  }
+}
+
+
+class Noticias{
+    
+set_Atributos( titulo , data , autor , resumo , url ){
+  
+  this.titulo = titulo;
+  this.name = name;
+  this.data = data;
+  this.autor = autor;
+  this.resumo = resumo;
+  this.url = url;
+  
+}
+
+setNoticias(){
+  
+  if( this.title == undefined || this.data == undefined || this.autor == undefined || this.resumo == undefined || this.url == undefined ){
+    
+    throw new Mensagem_Error( "Alguns dados podem estar indefinidos");
+    
+  }else{    
+  return `<div id="container-noticias">
+          <h3>${this.titulo}</h3>
+          <h5>Publicado: ${this.data}</h1>
+          <h5>Autor: ${this.autor}</h1>
+          <p>${this.resumo}</p>
+          <center><a href="${this.url}">Clique aqui para ver a noticia completa</a></center>
+          </div>`;
+  }
+}
+
+ mostrar_Noticias(){
+  
+try{
+  
+     let id = document.getElementById("container-noticia_destaque");
+     return id.insertAdjacentHTML( 'afterend' , this.setNoticias() );
+  
+   }catch(error){
+    
+     let id = document.getElementById("noticias");
+     return id.insertAdjacentHTML( 'beforeend' , error.get_Message() );
+  
+    }
+  }
+}
